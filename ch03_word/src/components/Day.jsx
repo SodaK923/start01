2eaed159a6c6c72c0937a249ed0f9948e3ee4f10
Soda <k.sua923@gmail.com>
2025-05-12 16:33:59 +0900

    단어장(라우터까지)

---
+++ ch03_word/src/components/Day.jsx
@@ -0,0 +1,27 @@
+import { useParams } from 'react-router-dom';
+import dummy from '../db/data.json';
+
+export default function Day(){
+    // <Route path="/day/:day" element={<Day />} />
+    const {day} = useParams();
+    const wordList=dummy.words.filter(word=>word.day==day);
+
+    return(
+        <>
+            <h2>Day {day}</h2>
+            <table>
+                <tbody>
+                    {
+                        // dummy.words.map(word=>(
+                        wordList.map(word=>(
+                            <tr key={word.id}>
+                                <td>{word.eng}</td>
+                                <td>{word.kor}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </>
+    );
+}
