1ac76b9b43b4a36f56a6edb2a1bc249108e881e6
Soda <k.sua923@gmail.com>
2025-05-13 10:59:16 +0900

    rest: isDone 토글 json 실제 반영, 단어 삭제(loading 추가)

--- ch03_word/src/components/Day.jsx
+++ ch03_word/src/components/Day.jsx
@@ -1,20 +1,35 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import dummy from '../db/data.json';
 import Word from "./Word";
 
-export default function Day(){
+export default function Day() {
     // <Route path="/day/:day" element={<Day />} />
-    const {day} = useParams();
-    const wordList=dummy.words.filter(word=>word.day==day);
+    const { day } = useParams();
+    //console.log("받은 day 값:", day); 
+    const [words, setWords]=useState([]);
 
-    return(
+    useEffect(() => {
+        fetch(`http://localhost:3010/words?day=${day}`)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setWords(data);
+            });
+    }, [day]);
+
+    //const wordList = dummy.words.filter(word => word.day == day);
+    const wordList = words.filter(word => word.day == day);
+
+    return (
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
                     {
                         // dummy.words.map(word=>(
-                        wordList.map(word=>(
+                        wordList.map(word => (
                             <Word word={word} key={word.id} />
                         ))
                     }
